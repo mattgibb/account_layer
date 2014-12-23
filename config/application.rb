29ac8e13922b2df1ac1a06ee4d91f96e1e230f86
Matt Gibb <matt@mattgibb.com>
2014-12-22 18:57:22 -0800

    SQL schema dump

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,8 @@ module AccountLayer
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # sql schema dump
+    config.active_record.schema_format = :sql
   end
 end
