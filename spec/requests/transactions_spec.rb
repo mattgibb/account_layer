f8412650650214b2a938b0e56b4e0ef6da429d59
Matt Gibb <matt@mattgibb.com>
2015-01-05 19:44:28 +0100

    Fixed create transaction spec

--- spec/requests/transactions_spec.rb
+++ spec/requests/transactions_spec.rb
@@ -2,13 +2,14 @@ require "rails_helper"
 
 RSpec.describe "Transactions" do
   it_behaves_like "a resource", :transactions
+
   let(:debit_id)  { create(:debit_account).id }
   let(:credit_id) { create(:credit_account).id }
   let(:transaction_params) { {transaction: attributes_for(:transaction, credit_id: credit_id, debit_id: debit_id)} }
 
   context "without logging in" do
-    it { get_json  "/transactions"; expect(response.code).to eq "401" }
-    it { post_json "/transactions"; expect(response.code).to eq "401" }
+    it { get_json "/transactions"; expect(response.code).to eq "401" }
+    it { post_json "/transactions", transaction_params; expect(response.code).to eq "401" }
   end
 
   context "when logged in" do
