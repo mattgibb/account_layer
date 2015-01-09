869388e2799181b6687af9a282482b37e154b9db
Matt Gibb <matt@mattgibb.com>
2015-01-09 15:21:08 +0100

    Sorted db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,9 @@
-# No need for a Rails model, just bung the compound types in the db
 [
-  ['Origination Fees', 'ControlAccount', 'credit'],
-  ['Servicing Fees',   'ControlAccount', 'credit'],
-  ['Chargeoffs',       'ControlAccount', 'credit'],
-  ['Recoveries',       'ControlAccount', 'credit']
-].each do |name, type, credit_or_debit|
-  Account.find_or_create_by name: name, type: type, credit_or_debit: credit_or_debit
+  ['OriginationFees', 'credit'],
+  ['ServicingFees',   'credit'],
+  ['WellsFargoCash',  'debit'],
+  ['Chargeoffs',      'credit'],
+  ['Recoveries',      'credit']
+].each do |type, credit_or_debit|
+  Account.find_or_create_by type: type, credit_or_debit: credit_or_debit
 end
-
