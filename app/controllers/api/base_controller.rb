1582214b8f3f96ad087f9129147860a310f862e9
Matt Gibb <matt@mattgibb.com>
2015-02-20 20:02:25 -0800

    Get content-type and www-authenticate headers right

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -10,8 +10,18 @@ class API::BaseController < ActionController::Base
     end
 
     def authenticate
-      authenticate_or_request_with_http_token do |token, options|
+      authenticate_token || render_unauthorized
+    end
+
+    def authenticate_token
+      authenticate_with_http_token do |token, options|
         ENV["API_TOKEN"] == token
       end
+    end
+
+    def render_unauthorized
+      self.headers['WWW-Authenticate'] = 'Token realm="Application"'
+
+      render json: "Do one.", status: 401
     end
 end
